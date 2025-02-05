a20a46829a8fdd38a3ce628f26dc6e1f53df39b3
ISURIKA030 <169172844+ISURIKA030@users.noreply.github.com>
2025-02-05 06:36:54 +0000

    create productOverview function

--- cbc-Frontend/src/pages/home/productOverview.jsx
+++ cbc-Frontend/src/pages/home/productOverview.jsx
@@ -6,3 +6,28 @@ import ImageSlider from "../../components/imgeSlider";
 import { addToCart } from "../../utils/cartFunction";
 import toast from "react-hot-toast";
 
+export default function ProductOverview() {
+    const params = useParams();
+    const productId = params.id;
+    const [product, setProduct] = useState(null);
+    const [status, setStatus] = useState("loading"); //not-foud, found
+    const navigate = useNavigate();
+    useEffect(() => {
+      console.log(productId);
+      axios
+        .get(import.meta.env.VITE_BACKEND_URL + "/api/products/" + productId)
+        .then((res) => {
+          console.log(res.data);
+  
+          //if null
+          if (res.data == null) {
+            setStatus("not-found");
+          }
+  
+          if (res.data != null) {
+            setProduct(res.data);
+            setStatus("found");
+          }
+        });
+    }, []);
+  
