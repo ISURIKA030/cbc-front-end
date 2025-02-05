9f4ae9b0e790498f136344d5a467afb362ee9aa9
ISURIKA030 <169172844+ISURIKA030@users.noreply.github.com>
2025-02-05 05:23:16 +0000

    create a default function as cart( )

--- cbc-Frontend/src/pages/home/cart.jsx
+++ cbc-Frontend/src/pages/home/cart.jsx
@@ -4,3 +4,26 @@ import CartCard from "../../components/cartCard";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
 
+export default function Cart() {
+    const [cart, setCart] = useState([]);
+    const [total, setTotal] = useState(0);
+    const [labeledTotal, setLabeledTotal] = useState(0);
+    const navigate = useNavigate();
+    useEffect(() => {
+      setCart(loadCart());
+      console.log(loadCart());
+      axios
+        .post(import.meta.env.VITE_BACKEND_URL + "/api/orders/quote", {
+          orderedItems: loadCart(),
+        })
+        .then((res) => {
+          console.log(loadCart());
+          console.log(res.data);
+          if(res.data.total != null){
+            setTotal(res.data.total);
+            setLabeledTotal(res.data.total);
+          }
+        });
+    }, []);
+
+    
