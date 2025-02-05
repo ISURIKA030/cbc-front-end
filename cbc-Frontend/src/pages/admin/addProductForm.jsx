c6dc253311c7ed333bf19e465c1f685bb1dac9ed
ISURIKA030 <169172844+ISURIKA030@users.noreply.github.com>
2025-02-05 04:31:19 +0000

    import useNavigate hook

--- cbc-Frontend/src/pages/admin/addProductForm.jsx
+++ cbc-Frontend/src/pages/admin/addProductForm.jsx
@@ -1,3 +1,6 @@
 import axios from "axios";
 import { useState } from "react";
 import toast from "react-hot-toast";
+import { useNavigate } from "react-router-dom";
+import uploadMediaToSupabase from "../../utils/mediaUpload";
+
