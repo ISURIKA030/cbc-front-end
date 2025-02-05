43a87cd5ee6d0b98cc1906ae8fc902ee5305c3d2
ISURIKA030 <169172844+ISURIKA030@users.noreply.github.com>
2025-02-05 05:02:47 +0000

    import useState and useNavigate hook to adminProdctsPageand import react-icons and react-hot-toast

---
+++ cbc-Frontend/src/pages/admin/adminProductsPage.jsx
@@ -0,0 +1,7 @@
+import axios from "axios";
+import { useEffect,useState } from "react";
+import toast from "react-hot-toast";
+import { FaPlus,FaTrash} from "react-icons/fa";
+import {FaPencil} from "react-icons/fa6";
+import { Link , useNavigate } from "react-router-dom";
+
