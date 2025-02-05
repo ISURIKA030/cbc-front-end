5478005f1a73451feb8ac9dcf5ee3df4051787ec
ISURIKA030 <169172844+ISURIKA030@users.noreply.github.com>
2025-02-05 04:32:29 +0000

    create AddProductForm function

--- cbc-Frontend/src/pages/admin/addProductForm.jsx
+++ cbc-Frontend/src/pages/admin/addProductForm.jsx
@@ -4,3 +4,13 @@ import toast from "react-hot-toast";
 import { useNavigate } from "react-router-dom";
 import uploadMediaToSupabase from "../../utils/mediaUpload";
 
+export default function AddProductForm() {
+    const [productId, setProductId] = useState("");
+    const [productName, setProductName] = useState("");
+    const [alternativeNames, setAlternativeNames] = useState("");
+    const [imageFiles, setImageFiles] = useState([]);
+    const [price, setPrice] = useState("");
+    const [lastPrice, setLastPrice] = useState("");
+    const [stock, setStock] = useState("");
+    const [description, setDescription] = useState("");
+    const navigate = useNavigate()
