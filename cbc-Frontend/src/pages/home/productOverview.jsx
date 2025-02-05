ccb39b0179dccf9a625de30cbc6175003c525f73
ISURIKA030 <169172844+ISURIKA030@users.noreply.github.com>
2025-02-05 06:35:01 +0000

    import axios, useState, useNavigate , useParams hook

---
+++ cbc-Frontend/src/pages/home/productOverview.jsx
@@ -0,0 +1,8 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { useNavigate, useParams } from "react-router-dom";
+import ProductNotFound from "./productNotFound";
+import ImageSlider from "../../components/imgeSlider";
+import { addToCart } from "../../utils/cartFunction";
+import toast from "react-hot-toast";
+
