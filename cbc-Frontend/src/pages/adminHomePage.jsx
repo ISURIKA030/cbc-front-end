d7db9ec9c58d28158997f11ee3da846dfb08e192
ISURIKA030 <169172844+ISURIKA030@users.noreply.github.com>
2025-02-05 07:16:10 +0000

    create AdminHomePage function

--- cbc-Frontend/src/pages/adminHomePage.jsx
+++ cbc-Frontend/src/pages/adminHomePage.jsx
@@ -8,3 +8,35 @@ import { useEffect, useState } from "react";
 import toast from "react-hot-toast";
 import axios from "axios";
 
+export default function AdminHomePage() {
+    const [user,setUser] = useState(null)
+    const navigate = useNavigate();
+    useEffect(()=>{
+      const token = localStorage.getItem("token");
+      if (!token) {      
+        navigate("/login")
+        return;
+      }
+      axios
+        .get(import.meta.env.VITE_BACKEND_URL + "/api/users", {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        }).then((res)=>{
+          console.log(res.data)
+          if(res.data.type!="admin"){
+            toast.error("Unauthorized access")
+            navigate("/login")
+          }else{
+            setUser(res.data)
+          }
+  
+        }).catch((err)=>{
+          console.error(err)
+          toast.error("Failed to fetch user data")
+          navigate("/login")
+        })
+      
+    },[])
+
+    
