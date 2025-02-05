70f8b3836c5395498f2f2b50fa751f29ebb71897
ISURIKA030 <169172844+ISURIKA030@users.noreply.github.com>
2025-02-05 07:21:07 +0000

    return dashboard

--- cbc-Frontend/src/pages/adminHomePage.jsx
+++ cbc-Frontend/src/pages/adminHomePage.jsx
@@ -39,4 +39,57 @@ export default function AdminHomePage() {
       
     },[])
 
+    return (
+        <div className="bg-blue-200 w-full h-screen flex">
+          <div className="w-[20%] h-screen bg-blue-500 flex flex-col items-center py-4">
+            <Link 
+              className="flex flex-row items-center mb-4 text-white hover:text-blue-200" 
+              to="/admin/dashboard"
+            >
+              <BsGraphUp className="mr-2" /> Dashboard
+            </Link>
     
+            <Link 
+              className="flex flex-row items-center mb-4 text-white hover:text-blue-200" 
+              to="/admin/products"
+            >
+              <BsBoxSeam className="mr-2" /> Products
+            </Link>
+    
+            <Link 
+              className="flex flex-row items-center mb-4 text-white hover:text-blue-200" 
+              to="/admin/orders"
+            >
+              <BsCart4 className="mr-2" /> Orders
+            </Link>
+    
+            <Link 
+              className="flex flex-row items-center text-white hover:text-blue-200" 
+              to="/admin/customers"
+            >
+              <BsPeopleFill className="mr-2" /> Customers
+            </Link>
+          </div>
+    
+          <div className="w-[80%] h-screen ">
+    
+            {user!=null&&<Routes path="/*">
+              <Route path="/" element={<h1>Dashboard</h1>} />
+              <Route path="/products" element={<AdminProductsPage/>} />
+              <Route path="/products/addProduct" element={<AddProductForm/>} />
+              <Route path="/products/editProduct" element={<EditProductForm/>} />
+              <Route path="/orders" element={<AdminOrdersPage/>} />
+              <Route path="/customers" element={<h1>Customers</h1>} />
+              <Route path="/*" element={<h1>404 not found the admin page</h1>}/>
+            </Routes>}
+            {
+              user==null&&<div className="w-full h-full flex justify-center items-center">
+                {/* animating loading page */}
+                <div className="animate-spin rounded-full h-32 w-32 border-t-2 border-b-2 border-accent"></div>
+    
+              </div>
+            }
+          </div>
+        </div>
+      );
+    }
