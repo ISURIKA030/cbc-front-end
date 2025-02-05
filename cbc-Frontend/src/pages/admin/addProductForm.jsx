f26e6e50acf0fd3b352d15232732723cdf5a2837
ISURIKA030 <169172844+ISURIKA030@users.noreply.github.com>
2025-02-05 04:33:55 +0000

    add useNavigate hook

--- cbc-Frontend/src/pages/admin/addProductForm.jsx
+++ cbc-Frontend/src/pages/admin/addProductForm.jsx
@@ -14,3 +14,5 @@ export default function AddProductForm() {
     const [stock, setStock] = useState("");
     const [description, setDescription] = useState("");
     const navigate = useNavigate()
+
+    
