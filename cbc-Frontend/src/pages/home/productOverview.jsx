55cfc329fd009309c4ce97d175f20debad58a278
ISURIKA030 <169172844+ISURIKA030@users.noreply.github.com>
2025-02-05 06:41:27 +0000

    return products

--- cbc-Frontend/src/pages/home/productOverview.jsx
+++ cbc-Frontend/src/pages/home/productOverview.jsx
@@ -48,3 +48,67 @@ export default function ProductOverview() {
           }
         })
       }
+
+      return (
+        <div className="w-full h-[calc(100vh-100px)] ">
+          {status == "loading" && (
+            <div className="w-full h-full flex items-center justify-center">
+              <div className="animate-spin rounded-full h-32 w-32  border-2 border-gray-500 border-b-accent border-b-4"></div>
+            </div>
+          )}
+          {status == "not-found" && <ProductNotFound />}
+          {status == "found" && (
+            <div
+              className="w-full h-full flex flex-col lg:flex-row  items-center justify-center "
+            >
+              <h1 className="text-3xl font-bold text-gray-800 lg:hidden">
+                {product.productName}
+              </h1>
+              <p className="text-xl text-gray-600 lg:hidden">
+                {product.price > product.lastPrice && (
+                  <span className="line-through text-red-500">
+                    LKR.{product.price}
+                  </span>
+                )}{" "}
+                <span>{"LKR." + product.lastPrice}</span>
+              </p>
+              <div className="w-[100%]  border-[3px] border-blue-900 lg:w-[35%] lg:h-full">
+                <ImageSlider images={product.images} />
+              </div>
+              <div className="w-[65%] h-full p-4">
+                <h1 className="text-3xl font-bold text-gray-800 hidden lg:block">
+                  {product.productName}
+                </h1>
+                <h1 className="text-3xl font-bold text-gray-500">
+                  {product.altNames.join(" | ")}
+                </h1>
+                <p className="text-xl text-gray-600 hidden lg:block">
+                  {product.price > product.lastPrice && (
+                    <span className="line-through text-red-500">
+                      LKR.{product.price}
+                    </span>
+                  )}{" "}
+                  <span>{"LKR." + product.lastPrice}</span>
+                </p>
+                <p className="text-lg text-gray-600 line-clamp-3">
+                  {product.description}
+                </p>
+                <button
+                  onClick={onAddtoCartClick}
+                  className="bg-accent text-white p-2 rounded-lg"
+                >
+                  Add to cart
+                </button>
+                <button
+                  onClick={onBuyNowClick}
+                  className=" text-accent border mx-1 border-accent p-2 rounded-lg"
+                >
+                  Buy Now
+                </button>
+              </div>
+            </div>
+          )}
+        </div>
+      );
+    }
+    
